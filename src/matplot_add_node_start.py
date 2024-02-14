9b7dfbd5dd4060df186957f359400acff87e7fd1
anad5 <ana5duvnjak@gmail.com>
2024-02-14 17:06:35 -0500

    node updates and fb data

--- src/matplot_add_node_start.py
+++ src/matplot_add_node_start.py
@@ -1,30 +1,23 @@
-#ANAS VER.1
 import numpy as np
-import matplotlib.pyplot as plt; plt.close('all')
+import matplotlib.pyplot as plt
 import networkx as nx
 from matplotlib.animation import FuncAnimation
 import random as rd
 import matplotlib.cm as cm
 import matplotlib.colors as mcolors
-from util import generate_graph
+from util import generate_graph  # Assuming this import is correct and available
 
 # Added
 def animate_nodes(G, node_colors, scalarmappaple, colormap, pos=None, *args, **kwargs):
-
-    fig, ax = plt.subplots() 
+    fig, ax = plt.subplots()
     plt.title('Polya Urn Network')
 
-    # define graph layout if None given
+    #define graph layout
     if pos is None:
         pos = nx.spring_layout(G)
 
-    #draw graph
-    #plt.title('Polya Urn Network')
-    #cbar = plt.colorbar(scalarmappaple)
-    #cbar.set_label('Brand awareness')
-        
-    #initial
-    nodes = nx.draw_networkx_nodes(G, pos, node_color=node_colors[0, :], node_size = 100, cmap=colormap, ax=ax, *args, **kwargs)
+    # draw graph
+    nodes = nx.draw_networkx_nodes(G, pos, node_color=node_colors[0, :], node_size=100, cmap=colormap, ax=ax, *args, **kwargs)
     edges = nx.draw_networkx_edges(G, pos, ax=ax, *args, **kwargs)
 
     scalarmappaple.set_array(node_colors[0, :])
@@ -32,31 +25,17 @@ def animate_nodes(G, node_colors, scalarmappaple, colormap, pos=None, *args, **k
     cbar = fig.colorbar(scalarmappaple, ax=ax)  # Specify the ax argument
     cbar.set_label('Brand awareness', fontsize=12)
 
-    #rgba_array_i = scalarmappaple.to_rgba(node_colors[0,:])
-    #nodes = nx.draw_networkx_nodes(G, pos, node_color=rgba_array_i, cmap=colormap , *args, **kwargs)
-    #nodes = nx.draw_networkx_nodes(G, pos, cmap=colormap , *args, **kwargs)
-    #edges = nx.draw_networkx_edges(G, pos, *args, **kwargs)
-    #nodes.set_cmap(colormap)
-    #plt.axis('off')
-
-    #nodes.set_array(node_colors[0])
     def update(ii):
-        # nodes are just markers returned by plt.scatter;
-        # node color can hence be changed in the same way like marker colors\
-        rgba_array = scalarmappaple.to_rgba(node_colors[ii,:])
+        rgba_array = scalarmappaple.to_rgba(node_colors[ii, :])
+        nodes.set_array(node_colors[ii, :])
         nodes.set_color(rgba_array)
-        #nodes = nx.draw_networkx_nodes(G, pos, node_color=rgba_array, cmap=colormap , *args, **kwargs)
-        #test1 = np.expand_dims(test, axis=1)
-        #test2 = np.broadcast_to(test1, (test1.shape[0], 4))
-        #nodes.set_facecolor(test2)
-        #nodes.set_array(test)
+        scalarmappaple.set_array(node_colors[ii, :])
+        cbar.update_normal(scalarmappaple)
         return nodes,
 
-    #fig = plt.gcf()
-    frames=len(node_colors[:,0])
-    #animation = FuncAnimation(fig, update, interval=50, frames=len(node_colors[:,0]), blit=True)
+    frames = len(node_colors[:, 0])
     animation = FuncAnimation(fig, update, frames=frames, blit=True)
-    plt.close()
+    plt.close()  # Close the initial figure to prevent extra plots
     return animation
 
 # Added
@@ -73,26 +52,27 @@ def update_super(graph):
             graph.nodes[node]['super_blue'] += blue
             graph.nodes[node]['super_total'] += red + blue
 
-# Added
-def pull_ball(graph):
+# Updated
+def pull_ball(graph, influencer_probability=0.1):
     num_nodes = graph.number_of_nodes()
     for node in range(num_nodes):
-        random_pull = rd.uniform(0,1)
-        threshold = graph.nodes[node]['super_red']/graph.nodes[node]['super_total']
-        if random_pull < threshold: # Pulled a red ball
+        random_pull = rd.uniform(0, 1)
+        if node == influencer_node:
+            # The influencer node always pulls a red ball
+            graph.nodes[node]['red'] += delta_red
+            graph.nodes[node]['total'] += delta_red
+        elif random_pull < graph.nodes[node]['super_red'] / graph.nodes[node]['super_total']:
             graph.nodes[node]['red'] += delta_red
             graph.nodes[node]['total'] += delta_red
         else:
             graph.nodes[node]['blue'] += delta_blue
             graph.nodes[node]['total'] += delta_blue
-        graph.nodes[node]['health'].append((graph.nodes[node]['red']/graph.nodes[node]['total'])) # Update the health of each node
-        #graph.nodes[node]['health'].append(int((graph.nodes[node]['red']/graph.nodes[node]['total'])*100)) # Update the health of each node
+        graph.nodes[node]['health'].append((graph.nodes[node]['red'] / graph.nodes[node]['total']))
 
-# Added
-def init_urns(graph):
+# Updated
+def init_urns(graph, influencer_node):
     num_nodes = graph.number_of_nodes()
     for node in range(num_nodes):
-        # i, red=2, blue=2, total=4, super_red=2, super_blue=2, super_total=4, health=[1], pos=(i,1)
         graph.nodes[node]['red'] = 2
         graph.nodes[node]['blue'] = 2
         graph.nodes[node]['total'] = 4
@@ -101,15 +81,28 @@ def init_urns(graph):
         graph.nodes[node]['super_total'] = 2
         graph.nodes[node]['health'] = [0.5]
 
-#graph = nx.complete_graph(total_nodes)
+    # Initialize influencer node
+    graph.nodes[influencer_node]['red'] = 4  # Influencer starts with all red balls
+    graph.nodes[influencer_node]['blue'] = 0
+    graph.nodes[influencer_node]['total'] = 4
+    graph.nodes[influencer_node]['super_red'] = 4
+    graph.nodes[influencer_node]['super_blue'] = 0
+    graph.nodes[influencer_node]['super_total'] = 4
+    graph.nodes[influencer_node]['health'] = [1.0]  # Influencer starts with all red balls
+
+    # Connect influencer node to existing nodes - connects it to all nodes.
+    for node in range(num_nodes):
+        graph.add_edge(influencer_node, node)
+
+# Example Usage
 graph = generate_graph("./src/graph_data/Fig5_1_c_Adjacency_Matrix.txt")
 time_steps = 40
 delta_red = 1
 delta_blue = 1
-num_iters = 10
 num_nodes = graph.number_of_nodes()
+influencer_node = 0  # Set influencer_node to a valid index (e.g., 0 for the first node)
 
-init_urns(graph)
+init_urns(graph, influencer_node)
 
 for i in range(time_steps):
     update_super(graph)
@@ -121,22 +114,15 @@ for node in range(num_nodes):
 
 health = np.array(health)
 
-node_colors_r = health[:,:-1].T
-node_colors_r_1 = health[:,:-1]
-node_colors_template = np.random.randint(0, 100, size=(time_steps, num_nodes))
+node_colors_r = health[:, :-1].T
 
-
-node_colors_test = np.ones((time_steps, num_nodes))
-
-#normalize = mcolors.Normalize(vmin=health.min(), vmax=health.max())
 normalize = mcolors.Normalize(vmin=0, vmax=1)
-#colormap = cm.jet
-colormap = mcolors.LinearSegmentedColormap.from_list("MyCmapName",["b","r"])
 colormap = plt.colormaps.get_cmap('seismic')
 
 scalarmappaple = cm.ScalarMappable(norm=normalize, cmap=colormap)
-scalarmappaple.set_array(health[0,:])
-
+scalarmappaple.set_array(health[0, :])
 
 animation = animate_nodes(graph, node_colors_r, scalarmappaple, colormap)
-animation.save('Test_2.gif', writer='imagemagick', savefig_kwargs={'facecolor':'white'}, fps=1)
+animation.save('Test_Influencer_Start_1.gif', writer='imagemagick', savefig_kwargs={'facecolor':'white'}, fps=1)
+
+plt.show()  # To display the final plot
