3ab1428e34e706787b944ad0dcc1719fbce275a3
Laeticia Niu <niulaeticia@gmail.com>
2024-02-14 15:35:06 -0500

    updated

--- src/lae_test.py
+++ src/lae_test.py
@@ -10,10 +10,10 @@ from util import generate_graph
 from graph_data import load_graph
 
 # Define paths to your files
-edge_path = '0 files/0.edges'
-feature_path = '0 files/0.feat'
-egofeat_path = '0 files/0.egofeat'
-ego_node = 0  # or whatever the ID of your ego node is
+edge_path = '348 files/348.edges'
+feature_path = '348 files/348.feat'
+egofeat_path = '348 files/348.egofeat'
+ego_node = 1912  # or whatever the ID of your ego node is
 
 graph = load_graph(edge_path, feature_path, egofeat_path, ego_node)
 
@@ -33,7 +33,7 @@ def animate_nodes(G, node_colors, scalarmappaple, colormap, pos=None, *args, **k
     #cbar.set_label('Brand awareness')
         
     #initial
-    nodes = nx.draw_networkx_nodes(G, pos, node_color=node_colors[0, :], node_size = 100, cmap=colormap, ax=ax, *args, **kwargs)
+    nodes = nx.draw_networkx_nodes(G, pos, node_color=node_colors[0, :], node_size = 75, cmap=colormap, ax=ax, *args, **kwargs)
     edges = nx.draw_networkx_edges(G, pos, ax=ax, *args, **kwargs)
 
     scalarmappaple.set_array(node_colors[0, :])
@@ -70,12 +70,27 @@ def animate_nodes(G, node_colors, scalarmappaple, colormap, pos=None, *args, **k
 
 # Added
 def update_super(graph):
-    num_nodes = graph.number_of_nodes()
-    for node in range(num_nodes):
+    #num_nodes = max(max(graph.nodes()), 1500) 
+    #for node in range(num_nodes):
+
+    for node in graph.nodes():
+        if 'red' not in graph.nodes[node]:
+            graph.nodes[node]['red'] = 2  # Default value, adjust as needed
+        if 'blue' not in graph.nodes[node]:
+            graph.nodes[node]['blue'] = 2  # Default value, adjust as needed
+
         graph.nodes[node]['super_red'] = graph.nodes[node]['red']
         graph.nodes[node]['super_blue'] = graph.nodes[node]['blue']
         graph.nodes[node]['super_total'] = graph.nodes[node]['red'] + graph.nodes[node]['blue']
+
+        
         for neighbor in graph.neighbors(node):
+
+            if 'red' not in graph.nodes[neighbor]:
+                graph.nodes[neighbor]['red'] = 2  # Again, initialize as appropriate
+            if 'blue' not in graph.nodes[neighbor]:
+                graph.nodes[neighbor]['blue'] = 2
+
             red = graph.nodes[neighbor]['red']
             blue = graph.nodes[neighbor]['blue']
             graph.nodes[node]['super_red'] += red
@@ -83,11 +98,30 @@ def update_super(graph):
             graph.nodes[node]['super_total'] += red + blue
 
 # Added
+def init_urns(graph):
+    #num_nodes = graph.number_of_nodes()
+    #for node in range(num_nodes):
+    for node in graph.nodes():
+        if node not in graph:
+            graph.add_node(node)
+        # i, red=2, blue=2, total=4, super_red=2, super_blue=2, super_total=4, health=[1], pos=(i,1)
+        graph.nodes[node]['red'] = 2
+        graph.nodes[node]['blue'] = 2
+        graph.nodes[node]['total'] = 4
+
+        graph.nodes[node]['super_red'] = 2
+        graph.nodes[node]['super_blue'] = 2
+        graph.nodes[node]['super_total'] = 2
+        graph.nodes[node]['health'] = [0.5]
+
+# Added
 def pull_ball(graph):
-    num_nodes = graph.number_of_nodes()
-    for node in range(num_nodes):
+    #num_nodes = max(max(graph.nodes()), 1500) 
+    #for node in range(num_nodes):
+    for node in graph.nodes():
         random_pull = rd.uniform(0,1)
         threshold = graph.nodes[node]['super_red']/graph.nodes[node]['super_total']
+
         if random_pull < threshold: # Pulled a red ball
             graph.nodes[node]['red'] += delta_red
             graph.nodes[node]['total'] += delta_red
@@ -97,18 +131,6 @@ def pull_ball(graph):
         graph.nodes[node]['health'].append((graph.nodes[node]['red']/graph.nodes[node]['total'])) # Update the health of each node
         #graph.nodes[node]['health'].append(int((graph.nodes[node]['red']/graph.nodes[node]['total'])*100)) # Update the health of each node
 
-# Added
-def init_urns(graph):
-    num_nodes = graph.number_of_nodes()
-    for node in range(num_nodes):
-        # i, red=2, blue=2, total=4, super_red=2, super_blue=2, super_total=4, health=[1], pos=(i,1)
-        graph.nodes[node]['red'] = 2
-        graph.nodes[node]['blue'] = 2
-        graph.nodes[node]['total'] = 4
-        graph.nodes[node]['super_red'] = 2
-        graph.nodes[node]['super_blue'] = 2
-        graph.nodes[node]['super_total'] = 2
-        graph.nodes[node]['health'] = [0.5]
 
 #graph = nx.complete_graph(total_nodes)
 #graph = generate_graph("./src/graph_data/Fig5_1_c_Adjacency_Matrix.txt")
@@ -124,14 +146,38 @@ for i in range(time_steps):
     update_super(graph)
     pull_ball(graph)
 
+# health = np.empty((num_nodes, time_steps+1))
+# #for node in range(num_nodes):
+# for node in graph.nodes():
+#     health[node] = graph.nodes[node]['health']
+
+# health = np.array(health)
+
+# node_colors_r = health[:,:-1].T
+# node_colors_r_1 = health[:,:-1]
+
+# Initialize a mapping from node IDs to indices
+node_to_index = {node: i for i, node in enumerate(graph.nodes())}
+
+# Initialize the health array based on the number of nodes
+num_nodes = len(graph.nodes())
 health = np.empty((num_nodes, time_steps+1))
-for node in range(num_nodes):
-    health[node] = graph.nodes[node]['health']
 
-health = np.array(health)
+# Use the mapping when accessing the health array
+for node in graph.nodes():
+    index = node_to_index[node]  # Convert node ID to index
+    health[index, :] = graph.nodes[node]['health']
 
-node_colors_r = health[:,:-1].T
-node_colors_r_1 = health[:,:-1]
+# No need to convert to np.array, health is already a NumPy array
+# health = np.array(health)
+
+node_colors_r = health[:, :-1].T  # Use slicing to exclude the last column for all rows
+node_colors_r_1 = health[:, :-1]  # This is the same as node_colors_r without transposition
+
+# Assuming node_colors_template is to be used for some other purpose and unrelated to the node-to-index mapping
+node_colors_template = np.random.randint(0, 100, size=(time_steps, num_nodes))
+
+
 node_colors_template = np.random.randint(0, 100, size=(time_steps, num_nodes))
 
 
@@ -149,4 +195,4 @@ scalarmappaple.set_array(health[0,:])
 
 
 animation = animate_nodes(graph, node_colors_r, scalarmappaple, colormap)
-animation.save('test0.gif', writer='imagemagick', savefig_kwargs={'facecolor':'white'}, fps=1)
+animation.save('test348.gif', writer='imagemagick', savefig_kwargs={'facecolor':'white'}, fps=1)
